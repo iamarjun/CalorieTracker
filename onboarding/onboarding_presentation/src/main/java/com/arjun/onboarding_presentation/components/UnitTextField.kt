bf068b8d890dc2af05ee9fa3ebaf56f140444ef9
iamarjun <arjunmanoj1995@gmail.com>
2023-03-12 09:43:13 +0530

    Onboarding components

---
+++ onboarding/onboarding_presentation/src/main/java/com/arjun/onboarding_presentation/components/UnitTextField.kt
@@ -0,0 +1,54 @@
+package com.arjun.onboarding_presentation.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.LastBaseline
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.sp
+import com.arjun.core_ui.LocalSpacing
+
+@Composable
+fun UnitTextField(
+    modifier: Modifier,
+    value: String,
+    onValueChange: (String) -> Unit,
+    unit: String,
+    textStyle: TextStyle = TextStyle(
+        color = MaterialTheme.colors.primaryVariant,
+        fontSize = 70.sp,
+    )
+) {
+    val spacing = LocalSpacing.current
+    Row(
+        modifier = modifier,
+        horizontalArrangement = Arrangement.Center
+    ) {
+        BasicTextField(
+            value = value,
+            onValueChange = onValueChange,
+            modifier = modifier
+                .width(IntrinsicSize.Min)
+                .alignBy(LastBaseline),
+            textStyle = textStyle,
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Number
+            ),
+            singleLine = true,
+        )
+        Spacer(modifier = modifier.width(spacing.spaceSmall))
+        Text(
+            text = unit,
+            modifier = modifier.alignBy(LastBaseline)
+        )
+    }
+}
